19417125789f8699a64154566116cdfdaa73305b
Carmel <carmel13@meet.mit.edu>
2014-08-11 18:28:05 +0300

    noy files

--- main.py
+++ main.py
@@ -11,33 +11,15 @@ from kivy.lang import Builder
 
 sm = ScreenManager()
 
-#screens = [Screen(name='Title {}'.format(i)) for i in range(2)]
-
-#sm.switch_to(screens[0])
-#sm.switch_to(screens[1], direction='right')
-
-#for i in range(2):
-#	screen = Screen(name='Title %d' % i)
-#	sm.add_widget(screen)
-
-#sm.current = 'Title 2'
-
-class FindVol(Screen):
-	pass
 class NewsFeed(Screen):
 	pass
 
-#sm.add_widget(FindVol(name='FindAVolunteer'))
-#sm.add_widget(NewsFeed(name='NewsFeed'))	
 
 class FFTApp(App):
 	def build(self):
 		self.news = NewsFeed()
-		sm.add_widget(news)
-		self.find = FindVol()
+		sm.add_widget(self.news)
 		return sm
-	def change(self):
-
 
 if __name__ == '__main__':
 	FFTApp().run()
